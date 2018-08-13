04dd6a55a3b685641109b3902961d49962331495
Yevhenii Liashenko <yev.lnko@gmail.com>
2018-08-13 23:38:25 +0300

    ps #2

--- source/index.js
+++ source/index.js
@@ -4,6 +4,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter as Router } from 'react-router-redux';
 
+
 // Instruments
 import './theme/init';
 import { store, history } from "./init/store";
